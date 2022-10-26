d897d96b5653f25a0cf5870065454adfe73ae0b3
Andre Ostan <andre.ostan@gmail.com>
2022-10-26 19:37:06 -0300

    Using the Express Router

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,38 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { router } from './routes/bootcamps.js';
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  // res.send('<h1>Hello from express</h1>');
-  // res.json({ name: 'Andre' });
-  // res.sendStatus(400);
-  // res.status(400).json({ sucess: false });
-  res.status(200).json({ sucess: true, msg: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ sucess: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ sucess: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// Mount bootcamps
+app.use('/api/v1/bootcamps', router);
 
 const PORT = process.env.PORT || 5000;
 

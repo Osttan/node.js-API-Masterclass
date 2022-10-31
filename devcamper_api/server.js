0928a62fbf4a901dbe1867b938eaf1b44a87e607
Andre Ostan <andre.ostan@gmail.com>
2022-10-31 19:37:50 -0300

    19. Connecting to the Database with Mongoose

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 // import { logger } from './middleware/logger.js';
 import morgan from 'morgan';
-import { router } from './routes/bootcamps.js';
+// import { router } from './routes/bootcamps.js';
+import { connectDB } from './config/db.js';
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -16,11 +20,20 @@ if (process.env.NODE_ENV === 'development') {
 // app.use(logger);
 
 // Mount bootcamps
-app.use('/api/v1/bootcamps', router);
+// app.use('/api/v1/bootcamps', router);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', err => {
+  console.log(`Error: ${err.message}`);
+  // CLose server & exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});

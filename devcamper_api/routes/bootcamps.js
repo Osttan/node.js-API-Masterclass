f6cc68b68e22423231369d676d353c623ab7dfff
Andre Ostan <andre.ostan@gmail.com>
2022-10-31 16:24:49 -0300

    Creating Controller Methods

--- devcamper_api/routes/bootcamps.js
+++ devcamper_api/routes/bootcamps.js
@@ -1,28 +1,44 @@
 import express from 'express';
+import {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} from '../controllers/bootcamps.js';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ sucess: true, msg: 'Show all bootcamps' });
-});
+router.route('/').get(getBootcamps).post(createBootcamp);
 
-router.get('/:id', (req, res) => {
-  res.status(200).json({ sucess: true, msg: `Show bootcamp ${req.params.id}` });
-});
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
-router.post('/', (req, res) => {
-  res.status(200).json({ sucess: true, msg: 'Create new bootcamp' });
-});
+// router.get('/', (req, res) => {
+//   res.status(200).json({ sucess: true, msg: 'Show all bootcamps' });
+// });
 
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Update bootcamp ${req.params.id}` });
-});
+// router.get('/:id', (req, res) => {
+//   res.status(200).json({ sucess: true, msg: `Show bootcamp ${req.params.id}` });
+// });
 
-router.delete('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// router.post('/', (req, res) => {
+//   res.status(200).json({ sucess: true, msg: 'Create new bootcamp' });
+// });
+
+// router.put('/:id', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ sucess: true, msg: `Update bootcamp ${req.params.id}` });
+// });
+
+// router.delete('/:id', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ sucess: true, msg: `Delete bootcamp ${req.params.id}` });
+// });
 
 export { router };

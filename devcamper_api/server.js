acb21b6e9d152522501ad0a0de624cc3c53d72ae
Andre Ostan <andre.ostan@gmail.com>
2022-10-31 16:49:53 -0300

    Intro to Middleware

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+// import { logger } from './middleware/logger.js';
+import morgan from 'morgan';
 import { router } from './routes/bootcamps.js';
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev loggind middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+// app.use(logger);
 
 // Mount bootcamps
 app.use('/api/v1/bootcamps', router);

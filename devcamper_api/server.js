8652800961fb40ca04844f0bfcdec3fa0596f3c5
Andre Ostan <andre.ostan@gmail.com>
2022-11-01 18:11:03 -0300

    Reverting to 16. Intro to Middleware

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -2,15 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 // import { logger } from './middleware/logger.js';
 import morgan from 'morgan';
-import colors from 'colors';
-// import { router } from './routes/bootcamps.js';
-import { connectDB } from './config/db.js';
+import { router } from './routes/bootcamps.js';
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
-
-// Connect to database
-connectDB();
 
 const app = express();
 
@@ -21,22 +16,11 @@ if (process.env.NODE_ENV === 'development') {
 // app.use(logger);
 
 // Mount bootcamps
-// app.use('/api/v1/bootcamps', router);
+app.use('/api/v1/bootcamps', router);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(
+app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', err => {
-  console.log(`Error: ${err.message}`.red);
-  // CLose server & exit process
-  server.close(() => {
-    process.exit(1);
-  });
-});
